c184c1fa4691962c16ba57b9c00db7259d31187e
MariaJoseGa <majogafra@gmail.com>
2021-09-29 17:57:32 -0500

    master

--- MascotaFeliz.App.Dominio/Entidades/Mascota.cs
+++ MascotaFeliz.App.Dominio/Entidades/Mascota.cs
@@ -7,10 +7,11 @@ namespace MascotaFeliz.App.Dominio
      /// </summary>  
     public class Mascota
     {
+        ///Hola:) 
         public int  Id { get; set;}
         public string NombreMascota {get; set;}
         public string Raza {get;set;}
         public Tipo TipoAnimal {get;set;}
         public Propietario Propietario {get;set;}
     }
-}
+} 
